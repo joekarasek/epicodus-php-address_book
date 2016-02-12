58c962367583902f162c94795638587707e3d4e9
Joseph Karasek <joseph.karasek@gmail.com>
2016-02-12 08:09:16 -0800

    created properties for Contact

---
+++ src/Contact.php
@@ -0,0 +1,14 @@
+<?php
+    class Contact
+    {
+        //Properties
+        private $first_name;
+        private $last_name;
+        private $email_address;
+        private $street_address;
+        private $city;
+        private $zip_code;
+        private $state;
+        private $notes;
+    }
+?>
