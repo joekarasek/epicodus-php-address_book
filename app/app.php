ff5c47fd46c79dc3970c67653d1576fd0fad428c
Joseph Karasek <joseph.karasek@gmail.com>
2016-02-12 08:36:38 -0800

    link Contact and instantiate Silex and twig

--- app/app.php
+++ app/app.php
@@ -1,8 +1,21 @@
 <?php
+    // include dependencies and classes
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Contact.php";
 
+    // initialize super global $_SESSION w/ 'list_of_contacts'
+    session_start();
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
+    // instantiate Silex app, add twig capability to app
     $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+        ));
 
+    // routes
     $app->get("/", function() {
         return "Home";
     });
